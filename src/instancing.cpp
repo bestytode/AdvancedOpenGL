3e9a245e424f59f3892979a682513b8328021037
余振寰 <1932878552@qq.com>
2023-09-02 14:17:57 +0800

    1

--- src/instancing.cpp
+++ src/instancing.cpp
@@ -54,56 +54,7 @@ int main()
 		return -1;
 	}
 
-	//// Define translation array
-	//glm::vec2 translations[100];
-	//int index = 0;
-	//float offset = 0.1f;
-	//for (int y = -10; y < 10; y += 2) {
-	//	for (int x = -10; x < 10; x += 2) {
-	//		glm::vec2 translation;
-	//		translation.x = (float)x / 10.0f + offset;
-	//		translation.y = (float)y / 10.0f + offset;
-	//		translations[index++] = translation;
-	//	}
-	//}
-
-	//// store instance data in an array buffer
-	//unsigned int instanceVBO;
-	//glGenBuffers(1, &instanceVBO);
-	//glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	//// vertices config
-	//float quadVertices[] = {
-	//	// Positions     // Color 
-	//	-0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
-	//	 0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
-	//	-0.05f, -0.05f,  0.0f, 0.0f, 1.0f,
-
-	//	-0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
-	//	 0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
-	//	 0.05f,  0.05f,  0.0f, 1.0f, 1.0f
-	//};
-
-	//unsigned int VAO, VBO;
-	//glGenVertexArrays(1, &VAO);
-	//glGenBuffers(1, &VBO);
-	//glBindVertexArray(VAO);
-	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
-
-	//glEnableVertexAttribArray(0);
-	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	//glEnableVertexAttribArray(1);
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
-	//glEnableVertexAttribArray(2);
-	//glBindBuffer(GL_ARRAY_BUFFER, instanceVBO); // this attribute is from a different vertex buffer
-	//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
-	//glVertexAttribDivisor(2, 1); // important: this tell OpenGL an instanced vertex attribute.
-
-	// Load model
+	// Load model(s)
 	Model rock("res/models/rock/rock.obj");
 	Model mars("res/models/planet/planet.obj");
 
@@ -162,7 +113,7 @@ int main()
 	}
 
 	while (!glfwWindowShouldClose(window)) {
-		// Per frame logic
+		// Per-frame logic
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
